78d7b7b6e5794c2ec8f89a9cad46a5cac7e4ceab
BuildTools <unconfigured@null.spigotmc.org>
2023-01-12 18:03:08 -0800

    viscos and everyht

--- main.py
+++ main.py
@@ -3,47 +3,54 @@ import random
 import sys
 from typing import List
 import numpy as np
+import time
 
 import matplotlib.pyplot as plot
 
 # Constants
-GRAVITY_FORCE = -1
+GRAVITY_FORCE = -0.1
 WORLD_WIDTH_PIXELS = 128
 WORLD_HEIGHT_PIXELS = 128
 WALL_DAMP = 0.5
 TD = 1
 XOFFSET = 0
-YOFFSET = 0
-NUM_PARTICLES = 200
-MAX_DISTANCE = 3
+YOFFSET = 3
+NUM_PARTICLES = 500
+MAX_DISTANCE = 6
 MIN_DISTANCE = 0.5
-# PRESSURE_COEFFICIENT = 0.25
-PRESSURE_COEFFICIENT = 1
+PRESSURE_COEFFICIENT = 0.25
 maxCap = 10
-VISCOSITY = 0.2
+VISCOSITY = 0.005
+BOUNCEBACK = 0.2
+BOUNCEBACKX = 5
+BELOWX = -3
+BELOWY = - 3
+
+plot.ion()
+# Allows the X button to work. Don't worry about how this works exactly unless you are interested
+plot.gcf().canvas.mpl_connect('close_event', lambda event: sys.exit())
 
 
 class ParticleList:
     def __init__(self):
-        self.x_pos = np.zeros(NUM_PARTICLES, dtype=np.float32)
-        self.y_pos = np.zeros(NUM_PARTICLES, dtype=np.float32)
+        self.x_pos = np.random.random(NUM_PARTICLES) * WORLD_WIDTH_PIXELS
+        self.y_pos = np.random.random(NUM_PARTICLES) * WORLD_HEIGHT_PIXELS
         self.x_vel = np.zeros(NUM_PARTICLES, dtype=np.float32)
         self.y_vel = np.zeros(NUM_PARTICLES, dtype=np.float32)
         self.force_y = np.zeros(NUM_PARTICLES, dtype=np.float32)
         self.force_x = np.zeros(NUM_PARTICLES, dtype=np.float32)
 
-    def update_particles(self):
+    def update_particles(self, sec, sleep):
         """
         Calculates density of particles
             Density is calculated by summing the relative distance of neighboring particles
         """
         #     # Density is calculated by summing the relative distance of neighboring particles
-        #     for particle_2 in particles_list:
 
         for i in range(NUM_PARTICLES):
             distance = np.hypot((self.x_pos - self.x_pos[i]), (self.y_pos - self.y_pos[i]))
 
-            close_enough = MIN_DISTANCE < distance[i] & distance[i] < MAX_DISTANCE
+            close_enough = np.logical_and((MIN_DISTANCE < distance), (distance < MAX_DISTANCE))
 
             normal_distance = 1 - distance / MAX_DISTANCE
 
@@ -52,64 +59,60 @@ class ParticleList:
             pressurex = PRESSURE_COEFFICIENT * (pressure * (self.x_pos[i] - self.x_pos))
             pressurey = PRESSURE_COEFFICIENT * (pressure * (self.y_pos[i] - self.y_pos))
 
-            viscosityx = VISCOSITY * math.pow(self.x_vel[i] - self.x_vel, 1) / distance
-            viscosityy = VISCOSITY * math.pow(self.y_vel[i] - self.y_vel, 1) / distance
+            viscosityx = VISCOSITY * np.power(self.x_vel[i] - self.x_vel[close_enough], 2) / distance[close_enough]
+            viscosityy = VISCOSITY * np.power(self.y_vel[i] - self.y_vel[close_enough], 2) / distance[close_enough]
 
             self.force_y[i] += np.sum(pressurey[close_enough])
             self.force_x[i] += np.sum(pressurex[close_enough])
 
-            self.force_y[i] += np.sum(viscosityy[close_enough])
-            self.force_x[i] += np.sum(viscosityx[close_enough])
+            self.force_y[i] += np.sum(viscosityy)
+            self.force_x[i] += np.sum(viscosityx)
 
             # Calculate the Force pushing on this particle
-            force_x = 0
-            force_y = 0
-            force_y += GRAVITY_FORCE
 
-            # Calculate velocity from force
+        below0X = self.x_pos < BELOWX
+        aboveMaxX = self.x_pos > WORLD_WIDTH_PIXELS
 
-            # Move particle by the value of its velocity
+        below0Y = self.y_pos <= BELOWY
+        aboveMaxY = self.y_pos > WORLD_HEIGHT_PIXELS
 
-            particle.x_vel += force_x * TD
-            particle.y_vel += force_y * TD
-            particle.x_vel *= 0.95
-            particle.y_vel *= 0.95
-            particle.x_pos += particle.x_vel * TD
-            particle.y_pos += particle.y_vel * TD
-            if particle.x_pos < 0:
-                # force_x -= (particle.x_pos - 0) * WALL_DAMP
-                particle.x_vel = 0.2
-                particle.x_pos = XOFFSET
-                # force_x = force_x * -1
+        self.x_vel[aboveMaxX] = -BOUNCEBACKX
+        self.x_pos[aboveMaxX] = WORLD_WIDTH_PIXELS - BELOWX
+        self.x_vel[below0X] = BOUNCEBACKX
+        self.x_pos[below0X] = -1
 
-            # Same thing for the right wall
-            if particle.x_pos > WORLD_WIDTH_PIXELS:
-                # force_x -= (particle.x_pos - WORLD_WIDTH_PIXELS) * WALL_DAMP
-                particle.x_vel = -0.2
-                particle.x_pos = WORLD_WIDTH_PIXELS - XOFFSET
-                # force_x = force_x * -1
+        self.y_vel[aboveMaxY] = -BOUNCEBACK
+        self.y_pos[aboveMaxY] = WORLD_HEIGHT_PIXELS - BELOWY
+        self.y_vel[below0Y] = BOUNCEBACK
+        self.y_pos[below0Y] = -1
 
-            # Same thing but for the floor
-            if particle.y_pos <= 0:
-                # We use SIM_W instead of BOTTOM here because otherwise particles are too low
-                particle.y_vel = random.random() * 5
-                particle.y_pos = YOFFSET
-                # force_y = force_y * -1
+        self.force_y += GRAVITY_FORCE
 
-            if particle.y_pos > WORLD_HEIGHT_PIXELS:
-                # force_x -= (particle.x_pos - WORLD_WIDTH_PIXELS) * WALL_DAMP
-                particle.y_vel = -0.2
-                particle.y_pos = WORLD_HEIGHT_PIXELS - YOFFSET
+        # Calculate velocity from force
 
-    def draw(particles: List[Particle]):
-        x_positions = [particle.x_pos for particle in particles]
-        y_positions = [particle.y_pos for particle in particles]
+        # Move particle by the value of its velocity
 
+        self.x_vel += self.force_x * TD
+        self.y_vel += self.force_y * TD
+
+        self.x_vel *= 0.95
+        self.y_vel *= 0.95
+
+        self.x_pos += self.x_vel * TD
+        self.y_pos += self.y_vel * TD
+
+        self.force_x = np.zeros(NUM_PARTICLES, dtype=np.float32)
+        self.force_y = np.zeros(NUM_PARTICLES, dtype=np.float32)
+
+        if sleep:
+            time.sleep(sec)
+
+    def draw(self):
         # Clear the plot
         plot.cla()
         # Draw each particle according to x_position and y_positions
-        plot.scatter(x_positions, y_positions,
-                     s=4,  # Each particle is 4px
+        plot.scatter(self.x_pos, self.y_pos,
+                     s=30,  # Each particle is 4px
                      c='b'  # Color them blue
                      )
         # Fix the x and y range of the plot (or else they'll change based on the data)
@@ -118,55 +121,41 @@ class ParticleList:
         # Refresh with the new particles
         plot.pause(0.001)
 
-    def make_particles() -> List[Particle]:
-        # Spawn particles at random positions
-        particles_list: List[Particle] = []
 
-        for i in range(NUM_PARTICLES):
-            new_particle = Particle(random.random() * WORLD_WIDTH_PIXELS,
-                                    random.random() * WORLD_HEIGHT_PIXELS)
-            particles_list.append(new_particle)
-
-        return particles_list
-
-
-class QuadTree:
-    def __init__(self, xmin: float, ymin: float, xmax: float, ymax: float, particlesList: []):
-        self.particlesList = particlesList
-        self.ymax = ymax
-        self.xmax = xmax
-        self.ymin = ymin
-        self.xmin = xmin
-        self.northwest = None
-        self.southwest = None
-        self.southeast = None
-        self.northeast = None
-
-    def split(self):
-        if self.northwest == None and self.particlesList.size() > maxCap:
-            for _ in self.particlesList:
-                if self.xmin < _.x_pos < self.xmax / 2:
-                    _.add
-
-    def insert(self, quadtree):
-        for _ in self.particlesList:
-            if quadtree.xmin <= _.x_pos < quadtree.xmax / 2 and quadtree.ymin <= _.y_pos < quadtree.ymax / 2:
-                self.particlesList.add(_)
+# class QuadTree:
+#     def __init__(self, xmin: float, ymin: float, xmax: float, ymax: float, particlesList: []):
+#         self.particlesList = particlesList
+#         self.ymax = ymax
+#         self.xmax = xmax
+#         self.ymin = ymin
+#         self.xmin = xmin
+#         self.northwest = None
+#         self.southwest = None
+#         self.southeast = None
+#         self.northeast = None
+#
+#     # def split(self):
+#     #     if self.northwest is None and self.particlesList.size() > maxCap:
+#     #         for i in range(self.particlesList):
+#     #             if self.xmin < _.x_pos < self.xmax / 2:
+#     #
+#     #
+#     # def insert(self, quadtree):
+#     #     for _ in self.particlesList:
+#     #         if quadtree.xmin <= _.x_pos < quadtree.xmax / 2 and quadtree.ymin <= _.y_pos < quadtree.ymax / 2:
+#     #             self.particlesList.add(_)
 
 
-# Interactive Mode (Allow plots to be updated)
-plot.ion()
-# Allows the X button to work. Don't worry about how this works exactly unless you are interested
-plot.gcf().canvas.mpl_connect('close_event', lambda event: sys.exit())
+# Interactive Mode (Allow plots to be updated
 
 
 def main():
-    particles_list = make_particles()
+    particles = ParticleList()
 
     # Update loop
     while True:
-        draw(particles_list)
-        update_particles(particles_list)
+        particles.draw()
+        particles.update_particles(0.1, False)
 
 
 main()
